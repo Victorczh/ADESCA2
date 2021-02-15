6bb8f4aaf5d3dde396da650f1f63a741ae2a1cb5
Victor <VICTOR.19@ichat.sp.edu.sg>
2021-02-15 18:24:59 +0800

    .

--- app.js
+++ app.js
@@ -262,14 +262,7 @@ app.get('/company/queue', function (req,res,next) {
                 if(result == "") {
                     res.status(200).send(output);
                 }
-                        for (let a = 0; a < result.length; a++) {
-                            const status = 0
-                            if (result[a].status == true) {
-                                status = 1
-                            }
-                            output[a] = { queue_id: result[a].queue_id,is_active: status}
-                        }
-                    res.status(200).send(output);
+                    res.status(200).send(result);
             }
             else {
                 next({ body: { error: err.message, code: 'UNEXPECTED_ERROR' }, status: 500 });
